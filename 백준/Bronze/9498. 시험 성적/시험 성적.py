e87658b9a6b97aa6bdbc35025562fa1b832cdd31
SeongEunGyu <116744770+Dyedds@users.noreply.github.com>
2025-07-01 09:38:19 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+math_score = (int(input()))
+
+if(math_score >= 90):
+    print("A")
+elif(math_score >= 80):
+    print("B")
+elif(math_score >= 70):
+    print("C")
+elif(math_score >= 60):
+    print("D")
+else:
+    print("F")
